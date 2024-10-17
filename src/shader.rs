7f2112be5dd1e6d2c4d02756420e3d8911967dd8
wwIrvingww <mor22781@uvg.edu.gt>
2024-10-17 14:42:34 -0600

    feature: model matrix added in shader.rs file

--- src/shader.rs
+++ src/shader.rs
@@ -1,11 +1,9 @@
-
-
-use nalgebra_glm::{Vec3, Vec4};
+use nalgebra_glm::{Vec3, Vec4, Mat3};
 use crate::vertex::Vertex;
 use crate::Uniforms;
 
 pub fn vertex_shader(vertex: &Vertex, uniforms: &Uniforms) -> Vertex {
-  // Transform position
+  // Transformar posición del vértice utilizando la matriz de modelo
   let position = Vec4::new(
     vertex.position.x,
     vertex.position.y,
@@ -14,7 +12,7 @@ pub fn vertex_shader(vertex: &Vertex, uniforms: &Uniforms) -> Vertex {
   );
   let transformed = uniforms.model_matrix * position;
 
-  // Perform perspective division
+  // Dividir por w para obtener la coordenada NDC (Normalized Device Coordinates)
   let w = transformed.w;
   let transformed_position = Vec3::new(
     transformed.x / w,
@@ -22,15 +20,23 @@ pub fn vertex_shader(vertex: &Vertex, uniforms: &Uniforms) -> Vertex {
     transformed.z / w
   );
 
-  // Transform normal
+  // Crear una matriz 3x3 a partir de la matriz de modelo 4x4, ignorando la traslación
+  let model_matrix_3x3 = Mat3::new(
+    uniforms.model_matrix[0], uniforms.model_matrix[1], uniforms.model_matrix[2],
+    uniforms.model_matrix[4], uniforms.model_matrix[5], uniforms.model_matrix[6],
+    uniforms.model_matrix[8], uniforms.model_matrix[9], uniforms.model_matrix[10]
+  );
 
-  // Create a new Vertex with transformed attributes
+  // Transformar la normal utilizando solo la parte rotacional de la matriz de modelo
+  let transformed_normal = (model_matrix_3x3 * vertex.normal).normalize();
+
+  // Crear un nuevo vértice con los atributos transformados
   Vertex {
     position: vertex.position,
     normal: vertex.normal,
     tex_coords: vertex.tex_coords,
     color: vertex.color,
     transformed_position,
-    transformed_normal: vertex.normal,
+    transformed_normal,
   }
 }
