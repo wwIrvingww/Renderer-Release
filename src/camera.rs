916c90822de326bfb1cb6a726c1f410e957311ee
wwIrvingww <mor22781@uvg.edu.gt>
2024-11-10 15:38:36 -0600

    try: pseudo pitch and yaw added

--- src/camera.rs
+++ src/camera.rs
@@ -5,8 +5,11 @@ pub struct Camera {
   pub eye: Vec3,
   pub center: Vec3,
   pub up: Vec3,
-  pub has_changed: bool
+  pub yaw: f32,
+  pub pitch: f32,
+  pub has_changed: bool,
 }
+
 
 impl Camera {
   pub fn new(eye: Vec3, center: Vec3, up: Vec3) -> Self {
@@ -14,6 +17,8 @@ impl Camera {
       eye,
       center,
       up,
+      yaw: 0.0,   // Inicializa yaw a 0.0
+      pitch: 0.0, // Inicializa pitch a 0.0
       has_changed: true,
     }
   }
@@ -39,23 +44,22 @@ impl Camera {
     let radius_vector = self.eye - self.center;
     let radius = radius_vector.magnitude();
 
-    let current_yaw = radius_vector.z.atan2(radius_vector.x);
+    self.yaw = (radius_vector.z.atan2(radius_vector.x) + delta_yaw) % (2.0 * PI);
 
     let radius_xz = (radius_vector.x * radius_vector.x + radius_vector.z * radius_vector.z).sqrt();
-    let current_pitch = (-radius_vector.y).atan2(radius_xz);
-
-    let new_yaw = (current_yaw + delta_yaw) % (2.0 * PI);
-    let new_pitch = (current_pitch + delta_pitch).clamp(-PI / 2.0 + 0.1, PI / 2.0 - 0.1);
+    self.pitch = (-radius_vector.y).atan2(radius_xz) + delta_pitch;
+    self.pitch = self.pitch.clamp(-PI / 2.0 + 0.1, PI / 2.0 - 0.1);
 
     let new_eye = self.center + Vec3::new(
-      radius * new_yaw.cos() * new_pitch.cos(),
-      -radius * new_pitch.sin(),
-      radius * new_yaw.sin() * new_pitch.cos()
+      radius * self.yaw.cos() * self.pitch.cos(),
+      -radius * self.pitch.sin(),
+      radius * self.yaw.sin() * self.pitch.cos()
     );
 
     self.eye = new_eye;
     self.has_changed = true;
   }
+
 
   pub fn zoom(&mut self, delta: f32) {
     let direction = (self.center - self.eye).normalize();
